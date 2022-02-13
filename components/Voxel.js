4d45177bd6e5841256ba29383aa047453f9526ae
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-13 14:13:30 +0900

    feat: fixed voxel

--- components/Voxel.js
+++ components/Voxel.js
@@ -51,7 +51,7 @@ const Voxel = () => {
                 </div>   
                 <div className={`voxel-viewer-wrap w-full h-full ${loaded ? 'opacity-100' : 'opacity-0'}  transition-all duration-700`}>
                     <model-viewer ref={view} src='/voxel/juni-coding-voxel.gltf' camera-controls  autoplay auto-rotate
-                    shadow-intensity="2.5" shadow-softness='1' alt='꾸생 Voxel Avatar' camera-orbit='150deg 65deg 110%' 
+                    shadow-intensity="2.5" shadow-softness='1' alt='꾸생 Voxel Avatar' camera-orbit='150deg 65deg 130%' 
                     environment-image='neutral' interaction-prompt='none' min-field-of-view='10deg' max-field-of-view='130deg'
                     max-camera-orbit="Infinity 180deg auto" min-camera-orbit='-Infinity 0deg auto' interpolation-decay='30'
                     auto-rotate-delay='0' rotation-per-second={speedTable[0]+'deg'} exposure='1'
