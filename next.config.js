6cd4d9a8de40bf681b7ef26ef4e9eba6f46cbea5
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-13 13:40:12 +0900

    feat: fixed img tag validation

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  }
 }
