516ef86fe148af2a4be322dcfe7ef39c13f16c71
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-15 10:12:47 +0900

    feat: remove lazyload comp

--- components/PortfolioItems.js
+++ components/PortfolioItems.js
@@ -1,12 +1,12 @@
 import Link from "next/link"
-import LazyImage from "./LazyImage"
+// import LazyImage from "./LazyImage"
 
 const PortfolioItems = ({post}) => {
     return(
         <article className="portfolio-item relative w-full rounded-lg overflow-hidden sm:hover:scale-[1.02] duration-[300] ease-linear transition-[transform] shadow-md">
             <Link href={`/portfolio/${post.node.postId}`} scroll={false} passHref>
             <a className="">
-                <LazyImage src={post.node.featuredImage.node.mediaItemUrl}
+                <img className="thumb-img block w-full" src={post.node.featuredImage.node.mediaItemUrl}
                     alt={post.node.featuredImage.node.altText === '' ? 'thumbnail_image' : post.node.featuredImage.node.altText}/>
 
                 <div className="item-cover absolute top-0 left-0 w-full h-full ">
