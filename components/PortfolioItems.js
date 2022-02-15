cc5ccffe261d5aa8618382257c60e523a79ddbbb
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-15 10:16:50 +0900

    feat: remove LazyImage como

--- components/PortfolioItems.js
+++ components/PortfolioItems.js
@@ -6,8 +6,11 @@ const PortfolioItems = ({post}) => {
         <article className="portfolio-item relative w-full rounded-lg overflow-hidden sm:hover:scale-[1.02] duration-[300] ease-linear transition-[transform] shadow-md">
             <Link href={`/portfolio/${post.node.postId}`} scroll={false} passHref>
             <a className="">
-                <img className="thumb-img block w-full" src={post.node.featuredImage.node.mediaItemUrl}
+                <img className="thumb-img block w-full opacity-1" src={post.node.featuredImage.node.mediaItemUrl}
                     alt={post.node.featuredImage.node.altText === '' ? 'thumbnail_image' : post.node.featuredImage.node.altText}/>
+
+                {/* <LazyImage src={post.node.featuredImage.node.mediaItemUrl}
+                    alt={post.node.featuredImage.node.altText === '' ? 'thumbnail_image' : post.node.featuredImage.node.altText}/> */}
 
                 <div className="item-cover absolute top-0 left-0 w-full h-full ">
                     <h3 className="absolute font-bold bottom-0 left-0 w-full pl-5 pb-5 text-2xl text-white">
