cd70dd3ba42a933348600bfab8ac93def0b57ee1
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-19 14:03:04 +0900

    feat: fixed css

--- pages/contact.js
+++ pages/contact.js
@@ -50,7 +50,7 @@ const Contact = () => {
             <HeadInfo title='Contact'/>
             <SubTitle txt={'Contact'} />
             <section className="contact-wrap pt-16">
-                <h3 className="text-5xl font-bold leading-[4rem]">
+                <h3 className="text-4xl sm:text-5xl font-bold leading-[4rem]">
                     방문해 주셔서<br className="lineBreak block sm:hidden"/> 감사합니다.
                 </h3>
                 <ul className="mt-10">
