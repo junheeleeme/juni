fdb6caa417fb454fbb2f10aeac37e33edc75da34
ju_ni <38034518+junheeleeme@users.noreply.github.com>
2022-02-27 16:04:45 +0900

    feat: fixed blog thumb css

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -3,10 +3,12 @@ import Link from "next/link"
 
 const BlogPost = ({post}) => {
     return(
-        <article className="blog-article mx-0 overflow-hidden rounded-md sm:hover:scale-[1.05] sm:transition-[transform] sm:duration-300 sm:    ease-in-out">
+        <article className="blog-article mx-0 overflow-hidden rounded-md sm:hover:scale-[1.05] sm:transition-[transform] sm:duration-300 sm: ease-in-out">
             <Link href={`https://juni-official.tistory.com${post.link}`} passHref>
                 <a className="" target="_blank">
-                    <img src={post.thumb} alt={post.title} className="rounded-md"/>
+
+                    <img src={post.thumb} alt={post.title} className="h-[300px] sm:h-[200px] w-full md:h-[135px] rounded-md"/>
+                    
                     <div className="relative h-18">
                         <h3 className="text-[1.15rem] md:text-[0.95rem] leading-6 pt-1.5 pb-0.5 pl-1 text-ellipsis whitespace-nowrap overflow-hidden">
                             {post.title}
@@ -14,6 +16,7 @@ const BlogPost = ({post}) => {
                         <span className="float-left px-1 py-0.5 text-[0.8rem]">{post.cate}</span>
                         <span className="float-right px-1 py-0.5 text-[0.8rem]">{post.date}</span>
                     </div>
+
                 </a>
             </Link>
         </article>
